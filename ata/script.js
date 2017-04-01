8d64ac09c92fab8e74bbb97e525123174720f63f
Ryan Zhang <Ryan778@users.noreply.github.com>
2017-04-01 12:11:04 -0600

    Add avoid the asteroids
    Also remove ckeditor dependency in Google Feud Answers

---
+++ ata/script.js
@@ -0,0 +1,10 @@
+function showInfo(n){
+    switch(n){
+        case 0: 
+            alertify.alert('<b>About</b><br>A minigame made back when I used Khan Academy for learning to code. <br>The original game was made in several hours back in 2013, which you can visit <a href="https://www.khanacademy.org/computer-programming/avoid-the-asteroids/2318174961" target="_blank">here</a>.<br>I stopped using KA for coding due to the super restrictive sandbox, and thus now putting my stuff here!');
+            break;
+        case 1:
+            alertify.alert('<b>Contact</b><br>Suggestions? Out of date answers? Contact me!<br>GitHub Issues: <a href="https://github.com/Ryan778/Ryan778.github.io/issues" target="_blank">Issues and/or Suggestions</a><br>Twitter: <a href="https://twitter.com/ryanz778" target="_blank">@ryanz778</a>')
+            break;
+    }
+}

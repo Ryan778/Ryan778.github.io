8b58449e237a413a2011dcfbb9986f6bf4de8e96
Ryan Zhang <Ryan778@users.noreply.github.com>
2017-05-16 19:26:09 -0600

    bkmusic: three new tracks
    also modified footer slightly

--- bkmusic/script.js
+++ bkmusic/script.js
@@ -2,12 +2,18 @@ var currentOption = 'rick';
 var previewTracks = {
   'sandstorm': 'https://archive.org/download/DarudeSandstorm_201411/Darude%20-%20Sandstorm.mp3',
   'rick': 'https://pixelcoding.nl/download/rickroll.mp3',
-  'sax': 'https://ryan778.github.io/bkmusic/tracks/sax.mp3'
+  'sax': 'https://ryan778.github.io/bkmusic/tracks/sax.mp3', 
+  'nyan': 'https://web.archive.org/web/20170517005627/http://www.nyan.cat/music/technyancolor.mp3', 
+  'tacos': 'https://ryan778.github.io/bkmusic/tracks/rainingtacos.mp3',
+  'whitenoise': 'https://ryan778.github.io/bkmusic/tracks/whitenoise.mp3'
 }
 var dlTracks = {
   'sandstorm': 'DarudeSandstorm.js',
   'rick': 'RickRoll.js',
-  'sax': 'EpicSaxGuy.js'
+  'sax': 'EpicSaxGuy.js',
+  'nyan': 'NyanCat.js',
+  'tacos': 'RainingTacos.js',
+  'whitenoise': 'WhiteNoise.js'
 }
 function selectOption(newopt){
   document.getElementById('opt_'+currentOption).className = 'option';
@@ -41,6 +47,14 @@ function preview(){
     previewbtn.innerHTML = "<i class='fa fa-play fa-fw'></i>";
     audio.pause();
   }
+}
+
+function showAbout(){
+  alertify.alert("<b>About</b><br>Inspired by <a href='http://www.latlmes.com/'>La Tlmes</a>, this site serves to allow people to harmlessly prank their friends, coworkers, and enemies. I don't take responsibility (you agreed to this when downloading the scripts) but will take suggestions and/or comments. <br><b>Contact Me:</b> <br>Twitter: <a href='https://twitter.com/ryanz778'>@ryanz778</a>, Mail: <a href='mailto:rjoker778@gmail.com'>rjoker778@gmail.com</a>")
+}
+
+function showDMCA(){
+  alertify.alert("<b>DMCA</b><br>Some tracks aren't hosted on this site, thus, contact the site owner hosting the tracks for DMCA takedown requests.<br><br>If a track is on my site, contact me at <a href='mailto:rjoker778@gmail.com'>rjoker778@gmail.com</a> for DMCA requests");
 }
 
 window.onload = function(){
---
+++ bkmusic/tacos.js
@@ -0,0 +1,24 @@
+/* RainingTacos.js
+Copyright (C) 2017 Ryan Zhang. 
+Licensed under CC-BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/)
+This means that you are free to modify it and redistribute it, but not removing the attribution (this text right here) or profiting from it. 
+
+To run, simply open the file. 
+To stop, simply restart the computer. 
+Once the script has been run, it can be deleted and the music will still play.
+*/
+
+var WShell = WScript.CreateObject('WScript.Shell');
+var popup = WShell.popup('Background Music Player Troll\nCopyright (C) Ryan Zhang 2017. Licensed under CC-BY-NC 4.0\n\nPress OK to start the script or CANCEL to abort the script.\nOnce the script starts, the file can be deleted.', 0, 'Background Music Player Troll (Raining Tacos)', 1)
+if(popup === 2){
+  WScript.Quit()
+}
+while (true){
+  var oPlayer = WScript.CreateObject('WMPlayer.OCX')
+  oPlayer.URL = 'https://ryan778.github.io/bkmusic/tracks/rainingtacos.mp3'
+  oPlayer.controls.play() 
+  while (oPlayer.playState != 1 ){
+    WScript.Sleep (100)
+  }
+  oPlayer.close()
+}
